15d45a373f20f32b3841a961b8d3c1ada50cc741
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2026-02-23 08:53:31 +0000

    Add project files.

---
+++ ToDoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoApp
+{
+    public class Program()
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("=========================");
+            Console.WriteLine("Welcome to ToDo App");
+            Console.WriteLine("=========================");
+        }
+    }
+}
